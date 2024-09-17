57d0b526aa9d459e4d6551a4b953587a9117696b
coleYab <yabume123@gamil.com>
2024-09-17 20:10:53 +0300

    add: added new items

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
+""" Simple Auth class to imulate authorization
+"""
 
 from typing import *
 from flask import request
 
 class Auth:
+    """ Auth clas that ecapsulates authing
+    """
     def __init__(self):
+        """ constructor type shit
+        """
         pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ requireing authing
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ authorizating the main header
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ getting current user
+        """
         return None
 
