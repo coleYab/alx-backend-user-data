e22d40860d8dbb08a8c93b726103c821bbc781e9
coleYab <yabume123@gamil.com>
2024-09-18 13:58:33 +0300

    add: adding extraction of header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,12 @@ class BasicAuth(Auth):
     """
     BasicAuth: basic auth system for class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.lstrip('Basic ')
