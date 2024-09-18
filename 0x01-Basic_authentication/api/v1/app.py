c5f25b438a19afcf253b7505c93eccf270a40e01
coleYab <yabume123@gamil.com>
2024-09-18 11:28:15 +0300

    fix: fixing pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,12 +14,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
