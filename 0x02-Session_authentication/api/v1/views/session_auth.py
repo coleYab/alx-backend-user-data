81ec28e2c2c816e30d4b97b27eb9688bd0adfe03
coleYab <yabume123@gamil.com>
2024-09-20 18:22:15 +0300

    add: implementing logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -7,29 +7,29 @@ from flask import jsonify, abort, request, make_response
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def user_login() -> str:
+def new_session() -> str:
     """ POST /api/v1/auth_session/login
     Return:
       - the status of the API
     """
     email = request.form.get('email')
     if email is None or not isinstance(email, str) or len(email) == 0:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     password = request.form.get('password')
     if password is None or not isinstance(password, str) or len(password) == 0:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
 
     from models.user import User
-    users = User.search({ 'email': email })
+    users = User.search({'email': email})
     if users is None or len(users) == 0:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     user = None
     for u in users:
         if u.is_valid_password(password):
             user = u
 
     if user is None:
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
 
     # creating user session form the auth value
     from api.v1.app import auth
@@ -42,3 +42,15 @@ def user_login() -> str:
     res.set_cookie(cookie_name, session_id)
 
     return res
+
+
+@app_views.route(
+                    '/auth_session/logout',
+                    methods=['DELETE'], strict_slashes=False)
+def destroy_session() -> str:
+    """
+    destroy_session: deleting the session for the user
+    """
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
