c96c65a1c0e9ea28c32fae195c1f1b1ad4fe19a3
coleYab <yabume123@gamil.com>
2024-09-21 16:51:03 +0300

    add: added session auth in db

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -27,6 +27,10 @@ if AUTH_TYPE == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
 
+if AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+
 
 @app.before_request
 def handler() -> None:
