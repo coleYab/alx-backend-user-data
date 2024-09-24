9039e99b444cb44fbf208be39d34d1cbe9af303b
coleYab <yabume123@gamil.com>
2024-09-24 18:15:56 +0300

    impl: adding db saving for our object

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Db: class for managing users and their shitty data
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from typing import TypeVar
+
+from user import Base, User
+
+
+class DB:
+    """ DB class
+    """
+
+    def __init__(self) -> None:
+        """ Initialize new db instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+        print("Created engine")
+
+    @property
+    def session(self) -> Session:
+        """ Memoized session object
+        """
+        if self.__session is None:
+            DBsession = sessionmaker(bind=self._engine)
+            self.__session = DBsession()
+        print("Created session")
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        new_user = User(email=email, hashed_password=hashed_password)
+        sess = self.session
+        sess.close_on_commit = False
+        sess.add(new_user)
+        sess.commit()
+        return new_user
+
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,8 +13,16 @@ class User(Base):
     """ User class impl by orm master
     """
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250))
     hashed_password = Column(String(250))
     session_id = Column(String(250))
     reset_token = Column(String(250))
+
+    def __repr__(self) -> str:
+        """
+        generating string representaiton for debugging
+        """
+        return f"User<id: {self.id}, email: {self.email}, hashed_password: {self.hashed_password}>"
+
+
