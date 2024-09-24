740056507ede837216394d208476dc7318d6de91
coleYab <yabume123@gamil.com>
2024-09-24 20:59:51 +0300

    add: adding a new user auth system

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 app: creating a simple application terminal
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
@@ -14,6 +16,18 @@ def homepage() -> str:
     """
     return jsonify({"message": "Bienvenue"}), 200
 
+@app.route('/users', methods=['POST'])
+def new_users() -> str:
+    """ new_users: adds a new user to our db
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+    except ValueError as e:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": email, "message": "user created"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port="5000")
