54e32945681067539fb41e73374f66251fa2bfc5
coleYab <yabume123@gamil.com>
2024-09-24 18:49:00 +0300

    add: adding user filters

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
-Db: class for managing users and their shitty data
+Db: class for managing user and their shitty data
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -40,3 +42,19 @@ class DB:
         sess.add(new_user)
         sess.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        find_user_by: this will search for user
+        """
+        user = None
+        try:
+            session = self._session
+            user = session.query(User).filter_by(**kwargs).first()
+        except InvalidRequestError:
+            raise InvalidRequestError
+
+        if user is None:
+            raise NoResultFound
+
+        return user
